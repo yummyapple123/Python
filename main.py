3d048c26660f690f7ec6f13e9b2d25e139e7f288
Van Nguyen <86445448+yummyapple123@users.noreply.github.com>
2022-12-14 16:36:50 +0000

    add a function with at least 2 parameters that takes 2 arguments

--- main.py
+++ main.py
@@ -3,15 +3,21 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
-def greet():
-  print("line 1")
-  print("line 2")
-  print("line 3")
+# def greet():
+#   print("line 1")
+#   print("line 2")
+#   print("line 3")
 
-greet()
+# greet()
 #Function with input
-def greet_with_name(name):
-  print(f"Hello {name}")
-  print(f"How do you do {name}?")
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}?")
 
-greet_with_name("Van")
+# greet_with_name("Van")
+
+# Function with multiple inputs
+def greet_with(name, location):
+  print(f"Hello there, I'm {name}")
+  print(f"I'm from {location}")
+greet_with("Van","Ottawa")
