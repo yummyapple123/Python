fca5692832f287f151a23c27171f751635665627
Van Nguyen <86445448+yummyapple123@users.noreply.github.com>
2022-12-14 17:30:51 +0000

    paint coverage calculator code

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
-# Review:
-# Create a function called greet().
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-
-# def greet():
-#   print("line 1")
-#   print("line 2")
-#   print("line 3")
-
-# greet()
-#Function with input
-# def greet_with_name(name):
-#   print(f"Hello {name}")
-#   print(f"How do you do {name}?")
-
-# greet_with_name("Van")
+#Write your code below this line 👇
+import math
+def paint_calc(height, width, cover):
+    result = math.ceil((test_h*test_w)/coverage)
+    print(f"You'll need {result} cans of paint")
 
 
-# Function with multiple inputs
-def greet_with(name, location):
-    print(f"Hello there, I'm {name}")
-    print(f"I'm from {location}")
 
 
-greet_with("Van", "Ottawa")
 
 
-# Keyword arguments
-def my_function(a=1, b=2, c=3):
-    print(a)
-    print(b)
-    print(c)
-def greet_with(name, location):
-    print(f"Hello there, I'm {name}")
-    print(f"I'm from {location}")
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
 
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
 
-greet_with(location="Ottawa", name="Van")
