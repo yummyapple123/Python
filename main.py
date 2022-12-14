a781c912433898484469e805ef202c86f2d900d6
Van Nguyen <86445448+yummyapple123@users.noreply.github.com>
2022-12-14 16:28:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("line 1")
+  print("line 2")
+  print("line 3")
+
+greet()
+#Function with input
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+
+greet_with_name("Van")
